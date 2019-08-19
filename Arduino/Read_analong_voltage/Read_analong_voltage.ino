c4e3f0687f218410dab659979588bcc3cf2dcb04
Robe <47053081+robegetachew@users.noreply.github.com>
2019-08-19 09:16:20 -0400

    Add files via upload

---
+++ Arduino/Read_analong_voltage/Read_analong_voltage.ino
@@ -0,0 +1,9 @@
+void setup() {
+  Serial.begin(9600);
+}
+void loop() {
+  int sensorValue = analogRead(A1);
+  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V)
+  float voltage = sensorValue * (5.0 / 1023.0);
+  Serial.println(voltage);
+}
