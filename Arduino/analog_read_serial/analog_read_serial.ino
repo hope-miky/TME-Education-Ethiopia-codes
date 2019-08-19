73bf7c8f21aad2c18d90920e301bf435f3ca3afe
Robe <47053081+robegetachew@users.noreply.github.com>
2019-08-19 09:08:45 -0400

    Add files via upload

---
+++ Arduino/analog_read_serial/analog_read_serial.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+}
+
+/// runs over and over again.
+void loop() {
+  // read the input on analog pin 1.
+  int sensorValue = analogRead(A1);
+  // print out the value you read.
+  Serial.println(sensorValue);
+  delay(1);        // delay in between reads.
+}
