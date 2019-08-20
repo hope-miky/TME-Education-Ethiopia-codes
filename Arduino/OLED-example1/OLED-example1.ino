b6d6c1183a5fa96b95692363e26dd27b3d409434
Tesfamichael <hope.miky1074@gmail.com>
2019-08-20 05:37:53 -0700

    added OLED display codes

---
+++ Arduino/OLED-example1/OLED-example1.ino
@@ -0,0 +1,57 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <hd44780.h>
+#include <hd44780ioClass/hd44780_I2Cexp.h>
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+hd44780_I2Cexp lcd(0x20, I2Cexp_MCP23008, 7, 6, 5, 4, 3, 2, 1, HIGH);
+
+
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+  lcd.clear(); 
+}
+
+void loop() {
+  
+  lcd.setCursor(0, 0);
+  lcd.print("TME Education");
+  lcd.setCursor(3, 1);
+  lcd.print("Ethiopia");
+  //testdrawbitmap(); 
+  //display.clearDisplay();
+  display.fillCircle(90, 30, 30, WHITE);
+  display.setTextColor(WHITE);
+  display.setTextSize(2.5);
+  display.setCursor(10,24); 
+  display.print("Blue");
+  
+  display.setTextColor(BLACK);
+  display.setTextSize(2.5);
+  display.setCursor(63,24); 
+  display.print("Moon");
+  display.display();
+  delay(1000);
+}
